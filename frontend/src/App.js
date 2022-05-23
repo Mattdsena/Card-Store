aa7a0c60b6531f3d34ef296ebfeb975afa5e0d5f
Matt DSena <matthewdsena@gmail.com>
2022-05-23 21:08:33 +0930

    got react to show a list of products i added in via data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Tickets Card Shop</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map((product) => (
+          <div key={product.slug}>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
