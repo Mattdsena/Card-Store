cab38731e23bd6e607f0eaf176b76ecc367aea5e
Matt DSena <matthewdsena@gmail.com>
2022-05-24 20:03:35 +0930

    single page need to not have a refresh, change from anchor to link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
       <div>
         <header>
-          <a href="/">Tickets Card Shop</a>
+          <Link to="/">Tickets Card Shop</Link>
         </header>
         <main>
           <Routes>
