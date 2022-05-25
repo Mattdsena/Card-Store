4052d5dd467edc75ee12eb18d5ee7bc44acb25dc
Matt DSena <matthewdsena@gmail.com>
2022-05-25 19:53:19 +0930

    changed colour of nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
         <header>
-          <Navbar bg="dark" variant="dark">
+          <Navbar bg="danger" variant=";ight">
             <Container>
               <LinkContainer to="/</Container>">
                 <Navbar.Brand>Tickets Card Shop</Navbar.Brand>
