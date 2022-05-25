4ba03c969636899ec3f4b608e9325002b6da48a7
Matt DSena <matthewdsena@gmail.com>
2022-05-25 20:12:32 +0930

    fixing issue with link

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,4 @@
 import { useEffect, useReducer, useState } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
