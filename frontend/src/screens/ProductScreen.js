7a9af5645499d898a7a05321a60ba6ff74eeb729
Matt DSena <matthewdsena@gmail.com>
2022-05-26 19:24:12 +0930

    imports

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,6 +3,11 @@ import { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import Rating from '../components/Rating';
 
 const reducer = (state, action) => {
   switch (action.type) {
